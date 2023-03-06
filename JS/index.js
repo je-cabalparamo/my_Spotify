50dbeb24a75e202390d4efec9235041c5f9a2e4b
unknown <je.cabalparamo@ugto.mx>
2023-03-06 11:22:57 -0600

    Acomodo de la pagina para canciones y buscador :)

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,7 @@
 const cardTop = document.querySelector('#cardTop').content
 const contenido = document.querySelector('#contenido')
 const fragment = document.createDocumentFragment()
+const btnBuscar = document.getElementById('buscador')
 let topTwoHundred = []
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -28,8 +29,25 @@ const creaCards = () => {
     topTwoHundred.forEach((song) => {
         cardTop.querySelector('img').setAttribute('src', song.trackMetadata.displayImageUri)
         cardTop.querySelector('.songname').textContent = song.trackMetadata.trackName
+        let artists = ''
+        let size = song.trackMetadata.artists.length
+        song.trackMetadata.artists.forEach((item, index) => {
+            if (index === size - 1) {
+                artists += item.name
+            } else {
+                artists += item.name + ' / '
+            }
+        })
+        cardTop.querySelector('.artistname').textContent = artists
+
+
         const clone = cardTop.cloneNode(true)
         fragment.appendChild(clone)
     })
     contenido.appendChild(fragment)
 }
+
+btnBuscar.addEventListener('keypress', () => {
+    //console.log('tecla', btnBuscar.value)
+    topTwoHundred
+})
